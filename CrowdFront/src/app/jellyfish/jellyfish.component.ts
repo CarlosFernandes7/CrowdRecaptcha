c7d76f98fe67647a622a16f1aa05ea5901d63986
Carlos Fernandes <al66669@utad.eu>
2023-12-16 22:40:54 +0000

    Integração com api e visualizacao de dados no component

--- CrowdFront/src/app/jellyfish/jellyfish.component.ts
+++ CrowdFront/src/app/jellyfish/jellyfish.component.ts
@@ -1,19 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiCrowdsourcingService } from '../api-crowdsourcing.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-jellyfish',
   templateUrl: './jellyfish.component.html',
-  styleUrls: ['./jellyfish.component.css'] // Corrigir para "styleUrls"
+  styleUrls: ['./jellyfish.component.css']
 })
-export class JellyfishComponent implements OnInit {
-  jellyfishData: any[] = [];
+export class JellyfishComponent implements OnInit, OnDestroy {
 
-  constructor(private apiCrowdsourcingService: ApiCrowdsourcingService) {}
+  backendData: any;
+  unknownJellyfishData: any; // New property for unknown jellyfish data
+  private ngUnsubscribe = new Subject<void>();
 
-  ngOnInit() {
-    this.apiCrowdsourcingService.getAllJellyfish().subscribe((data) => {
-      this.jellyfishData = data;
-    });
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.fetchData();
+    this.fetchUnknownJellyfishData(); // Call the new method
+  }
+
+  fetchData() {
+    this.http.get('http://localhost:3000/jellyfish')
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(
+        (data) => {
+          this.backendData = data;
+          console.log('Dados do backend:', this.backendData);
+        },
+        (error) => {
+          console.error('Erro ao obter dados do backend:', error);
+        }
+      );
+  }
+
+  fetchUnknownJellyfishData() {
+    this.http.get('http://localhost:3000/jellyfishunknown') // Adjust the endpoint accordingly
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(
+        (data) => {
+          this.unknownJellyfishData = data;
+          console.log('Dados do backend (Jellyfish Unknown):', this.unknownJellyfishData);
+        },
+        (error) => {
+          console.error('Erro ao obter dados do backend (Jellyfish Unknown):', error);
+        }
+      );
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
+  getImagemUrl(nomeImagem: string): string {
+    return `/assets/JellyFishConhecidos/${nomeImagem}`;
+  }
+
+  getImagemUrlDesconhecidos(nomeImagem: string): string {
+    return `/assets/JellyFishDesconhecidos/${nomeImagem}`;
   }
 }
