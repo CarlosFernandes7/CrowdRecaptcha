23aa3e18c6a5526f3597e6908f8cae718f1ee641
Carlos Fernandes <al66669@utad.eu>
2023-12-17 21:55:29 +0000

    Add ficheiro leiaME.txt

--- CrowdFront/src/app/jellyfish/jellyfish.component.ts
+++ CrowdFront/src/app/jellyfish/jellyfish.component.ts
@@ -4,61 +4,64 @@ import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
 
-
 @Component({
   selector: 'app-jellyfish',
   templateUrl: './jellyfish.component.html',
   styleUrls: ['./jellyfish.component.css']
 })
-
-
 export class JellyfishComponent implements OnInit, OnDestroy {
+  backendData: any; // Dados do backend para as jellyfish conhecidas
+  unknownJellyfishData: any; // Nova propriedade para os dados das jellyfish desconhecidas
+  private ngUnsubscribe = new Subject<void>(); // Sujeito para desinscrever observáveis quando o componente é destruído
+  jellyfishData: any; // Armazenar dados para a jellyfish selecionada
 
-  
-  backendData: any;
-  unknownJellyfishData: any; // New property for unknown jellyfish data
-  private ngUnsubscribe = new Subject<void>();
-  jellyfishData: any; // Store data for the selected jellyfish
+  constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
-
-  constructor(private route: ActivatedRoute, private http: HttpClient) {
- 
-  }
-  
-
-  
   ngOnInit(): void {
     console.log('ngOnInit chamado');
-    this.fetchData();
-    this.fetchUnknownJellyfishData();
-  
-    // Certifique-se de chamar após os dados serem carregados
+    this.fetchData(); // Buscar dados das jellyfish conhecidas
+    this.fetchUnknownJellyfishData(); // Buscar dados das jellyfish desconhecidas
+
+    // Certificar-se de chamar após os dados serem carregados
     setTimeout(() => {
       const dataLength = this.getUnknownJellyfishDataLength();
       this.selectedJellyfishId = this.generateRandomJellyfishId(1, dataLength);
-      this.fetchJellyfishById();
-    }, 50); // Ajuste o valor conforme necessário
+
+      this.fetchJellyfishById(); // Buscar dados da jellyfish selecionada
+    }, 1000); // Ajustar o valor conforme necessário
   }
-  
+
+  generateRandomJellyfishId(min: number, max: number): string {
+    // Verificar se o array de dados é válido e não está vazio
+    if (this.unknownJellyfishData && this.unknownJellyfishData.length > 0) {
+      // Verificar se min é menor que max
+      if (min < max) {
+        let randomIndex;
+        do {
+          randomIndex = Math.floor(Math.random() * (max - min + 1)) + min;
+          // Verificar se o índice gerado é válido
+        } while (randomIndex < 0 || randomIndex >= this.unknownJellyfishData.length);
+
+        const result = this.unknownJellyfishData[randomIndex].id.toString();
+        console.log('Número aleatório:', result);
+        return result;
+      } else {
+        console.error('O valor de min deve ser menor que max.');
+        return '';
+      }
+    } else {
+      console.error('Dados não disponíveis para gerar um ID aleatório.');
+      return '';
+    }
+  }
+
   getUnknownJellyfishDataLength(): number {
     if (this.unknownJellyfishData && Array.isArray(this.unknownJellyfishData)) {
       console.log('Dados foram exportados');
       return this.unknownJellyfishData.length;
     } else {
       console.error('Dados de jellyfish unknown não válidos ou não disponíveis.');
-      return 0; // Ou retorne um valor padrão conforme necessário
-    }
-  }
-
-  generateRandomJellyfishId(min: number, max: number): string {
-    if (this.unknownJellyfishData && this.unknownJellyfishData.length > 0) {
-      const randomIndex = Math.floor(Math.random() * (max - min + 1)) + min;
-      const result = this.unknownJellyfishData[randomIndex].id.toString();
-      console.log('Número aleatório:', result);
-      return result;
-    } else {
-      console.error('Dados não disponíveis para gerar um ID aleatório.');
-      return '';
+      return 0; // Ou retornar um valor padrão conforme necessário
     }
   }
 
@@ -77,13 +80,13 @@ export class JellyfishComponent implements OnInit, OnDestroy {
   }
 
   fetchUnknownJellyfishData() {
-    this.http.get('http://localhost:3000/jellyfishunknown') // Adjust the endpoint accordingly
+    this.http.get('http://localhost:3000/jellyfishunknown') // Ajustar o endpoint conforme necessário
       .pipe(takeUntil(this.ngUnsubscribe))
       .subscribe(
         (data) => {
           this.unknownJellyfishData = data;
 
-          // Inicialize a propriedade 'response' para cada jellyfishunknown
+          // Inicializar a propriedade 'response' para cada jellyfishunknown
           this.unknownJellyfishData.forEach((jellyfishunknown: any) => {
             jellyfishunknown.response = '';
           });
@@ -96,8 +99,6 @@ export class JellyfishComponent implements OnInit, OnDestroy {
       );
   }
 
-  
-
   selectedJellyfishId: string = "";
   selectedJellyfishData: any;
 
@@ -105,8 +106,8 @@ export class JellyfishComponent implements OnInit, OnDestroy {
     if (this.selectedJellyfishId) {
       this.fetchUnknownJellyfishById(this.selectedJellyfishId);
     } else {
-      console.error('Please enter a Jellyfish ID.');
-      // Handle the case where no ID is entered
+      console.error('Por favor, insira um ID de Jellyfish.');
+      // Lidar com o caso em que nenhum ID é inserido
     }
   }
 
@@ -118,20 +119,15 @@ export class JellyfishComponent implements OnInit, OnDestroy {
       .subscribe(
         (data) => {
           this.selectedJellyfishData = data;
-          this.selectedJellyfishData.response = ''; // Initialize the 'response' property
+          this.selectedJellyfishData.response = ''; // Inicializar a propriedade 'response'
 
-          console.log('Data for the selected Jellyfish Unknown:', this.selectedJellyfishData);
+          console.log('Dados para a Jellyfish Unknown selecionada:', this.selectedJellyfishData);
         },
         (error) => {
-          console.error('Error fetching data for the selected Jellyfish Unknown:', error);
+          console.error('Erro ao buscar dados para a Jellyfish Unknown selecionada:', error);
         }
       );
   }
-
-
-
-
-
 
   ngOnDestroy() {
     this.ngUnsubscribe.next();
@@ -148,30 +144,30 @@ export class JellyfishComponent implements OnInit, OnDestroy {
 
   submitResponse(jellyfishunknown: any) {
     console.log('Dados a serem enviados para inserirResposta:', jellyfishunknown.id, jellyfishunknown.response);
-  
-    // Certifique-se de que 'response' está presente em jellyfishunknown
+
+    // Certificar-se de que 'response' está presente em jellyfishunknown
     if (jellyfishunknown && 'response' in jellyfishunknown && jellyfishunknown.response) {
-      // Envie a resposta para o backend
+      // Enviar a resposta para o backend
       const resposta = {
         id_jellyfishunknown: jellyfishunknown.id,
         resposta_utilizador: jellyfishunknown.response
       };
-  
+
       this.http.post('http://localhost:3000/respostas', resposta)
         .pipe(takeUntil(this.ngUnsubscribe))
         .subscribe(
           (data) => {
             console.log('Resposta enviada com sucesso:', data);
-            // Faça qualquer outra ação necessária após o envio bem-sucedido
+            // Realizar qualquer outra ação necessária após o envio bem-sucedido
           },
           (error) => {
             console.error('Erro ao enviar resposta:', error);
-            // Lide com erros aqui
+            // Lidar com erros aqui
           }
         );
     } else {
       console.error('Resposta inválida.');
-      // Lide com casos onde a resposta é inválida
+      // Lidar com casos em que a resposta é inválida
     }
   }
 }
