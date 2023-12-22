d2a17fd97020226289ecb435bebde74c46ee6443
Carlos Fernandes <al66669@utad.eu>
2023-12-22 21:25:28 +0000

    add side bar

--- CrowdFront/src/app/jellyfish-list/jellyfish-list.component.ts
+++ CrowdFront/src/app/jellyfish-list/jellyfish-list.component.ts
@@ -1,38 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiCrowdsourcingService } from '../api-crowdsourcing.service';
 
+interface Jellyfish {
+  id: number;
+  nome: string;
+  descricao: string;
+}
+
 @Component({
   selector: 'app-jellyfish-list',
   templateUrl: './jellyfish-list.component.html',
   styleUrls: ['./jellyfish-list.component.css']
 })
 export class JellyfishListComponent implements OnInit {
-  jellyfishDescriptions: string[] = [];
+  jellyfishDescriptions: Jellyfish[] = [];
+  filteredJellyfish: Jellyfish[] = [];
+  loading: boolean = true;
+  error: boolean = false;
+  errorMessage: string = '';
+  searchTerm: string = '';
 
   constructor(private apiService: ApiCrowdsourcingService) {}
 
   ngOnInit(): void {
     this.fetchJellyfishDescriptions();
   }
-// Add these properties to your component class
-loading: boolean = true;
-error: boolean = false;
-errorMessage: string = '';
 
-fetchJellyfishDescriptions(): void {
-  this.loading = true;
-  this.apiService.getAllJellyfish().subscribe(
-    (data: { descricao: string[] }) => {
-      console.log('Dados recebid do back',data); // Verifique os dados no console
-      this.jellyfishDescriptions = data.descricao;
-      this.loading = false;
-    },
-    (error: any) => {
-      console.error('Erro ao obter as descrições das jellyfish:', error.status, error.message);
-      this.loading = false;
-      this.error = true;
-      this.errorMessage = 'Erro ao obter as descrições das jellyfish. Por favor, tente novamente.';
+  fetchJellyfishDescriptions(): void {
+    this.apiService.getAllJellyfish().subscribe(
+      (data: Jellyfish[]) => {
+        this.jellyfishDescriptions = data;
+        this.filteredJellyfish = [...this.jellyfishDescriptions];
+        this.loading = false;
+      },
+      (error: any) => {
+        console.error('Erro ao obter as descrições das jellyfish:', error.status, error.message);
+        this.loading = false;
+        this.error = true;
+        this.errorMessage = 'Erro ao obter as descrições das jellyfish. Por favor, tente novamente.';
+      }
+    );
+  }
+
+  filterJellyfish(): void {
+    // Filter jellyfish based on the search term
+    this.filteredJellyfish = this.jellyfishDescriptions.filter(jellyfish =>
+      jellyfish.nome.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  showDetails(jellyfish: Jellyfish): void {
+    const urlFromDescription = jellyfish.descricao;
+    if (urlFromDescription) {
+      window.open(urlFromDescription, '_blank');
     }
-  );
-}
+  }
 }
