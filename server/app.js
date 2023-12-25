c845a81fad7f7765274403367d6ec1da7937f5a7
Carlos Fernandes <al66669@utad.eu>
2023-12-25 19:13:24 +0000

    alterações na receção de imagem no servidor

--- server/app.js
+++ server/app.js
@@ -7,6 +7,15 @@ const path = require('path'); // Adicione esta linha para usar o módulo path
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 // Middleware for parsing JSON
 app.use(bodyParser.json());
 
